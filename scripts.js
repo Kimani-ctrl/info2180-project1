7085292d4805999758daa4b1013a677c7edf99d3
Kimani Allen <kimani2761469@hotmail.com>
2020-11-01 21:33:20 -0500

    Javascript code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-/* Add your JavaScript to this file */
+window.onload=function(){
+    (document.getElementsByTagName("button")[0]).onclick=function(event){
+        event.preventDefault();
+        if ((document.getElementsByTagName("input")[0]).value===""){
+            (document.getElementsByClassName("message")[0]).innerHTML="Please enter a valid email address";
+        }
+        else{
+            (document.getElementsByClassName("message")[0]).innerHTML="Thank you! your email address have been added to our mailing list!"
+        }
+    }
+}
